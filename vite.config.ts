e0953c6f475709ddf2e4ce10265091279f18b2cc
Wilson Malafaia Peixoto <wilsonmp2@gmail.com>
2026-08-15 18:41:29 -0300

    fix: remove opção obsoleta do bundler no Vite 8

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: './',
-    build: {
-        rollupOptions: {
-            maxParallelFileOps: 128,
-        },
-    },
+  base: './',
 });
