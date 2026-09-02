df26fc385a7c8a4436578ba414a45c45dee7a507
Wilson Malafaia Peixoto <wilsonmp2@gmail.com>
2026-09-02 17:15:12 -0300

    feat: ligar todas as dimensões de dados às rotas

--- src/product/App.tsx
+++ src/product/App.tsx
@@ -3,11 +3,15 @@ import { AppHeader } from './components/AppHeader';
 import { RouteEffects } from './components/RouteEffects';
 import { PortfolioProvider } from './PortfolioContext';
 import { AccountingOverviewPage } from './pages/AccountingOverviewPage';
+import { CoverageOverviewPage } from './pages/CoverageOverviewPage';
 import { BalancesOverviewPage } from './pages/BalancesOverviewPage';
 import { IndicatorPage } from './pages/IndicatorPage';
 import { IssuesOverviewPage } from './pages/IssuesOverviewPage';
+import { MonthlyEvolutionPage } from './pages/MonthlyEvolutionPage';
+import { MovementsOverviewPage } from './pages/MovementsOverviewPage';
 import { NotFoundPage } from './pages/NotFoundPage';
 import { PortfolioPage } from './pages/PortfolioPage';
+import { RegistrationOverviewPage } from './pages/RegistrationOverviewPage';
 import { RepasseOverviewPage } from './pages/RepasseOverviewPage';
 import { SchoolPage } from './pages/SchoolPage';
 import { SchoolsPage } from './pages/SchoolsPage';
@@ -38,8 +42,12 @@ export function App() {
               <Route path="/unidades/:inep" element={<SchoolPage />} />
               <Route path="/repasses" element={<RepasseOverviewPage />} />
               <Route path="/saldos" element={<BalancesOverviewPage />} />
+              <Route path="/evolucao" element={<MonthlyEvolutionPage />} />
+              <Route path="/movimentacoes" element={<MovementsOverviewPage />} />
+              <Route path="/cadastro" element={<RegistrationOverviewPage />} />
               <Route path="/pendencias" element={<IssuesOverviewPage />} />
               <Route path="/prestacao-contas" element={<AccountingOverviewPage />} />
+              <Route path="/cobertura" element={<CoverageOverviewPage />} />
               <Route path="/indicadores/:slug" element={<IndicatorPage />} />
               <Route path="*" element={<NotFoundPage />} />
             </Routes>
